412c8bf609fbae76d8356874007492aa576e4b1e
Jaemin Seol <43691888+JAEMINSEOL@users.noreply.github.com>
2025-12-09 17:47:12 +0900

    Update [위피] 유저 첫 결제 전후 젤리 사용 분석.sql

--- [위피] 유저 첫 결제 전후 젤리 사용 분석.sql
+++ [위피] 유저 첫 결제 전후 젤리 사용 분석.sql
@@ -8,11 +8,17 @@ with users as
                     where source_type ='MALE_NEW_USER_FIRST_JOIN_V1') t
         on t.user_id = u.user_id
     where gender=0
-        and first_approval_time between cast('2025-08-05 00:00:00'  as timestamp) - interval '9' hour and cast('2025-11-15 00:00:00' as timestamp) - interval '9' hour
+        and first_approval_time between cast('2025-08-05 00:00:00'  as timestamp) - interval '9' hour and cast('2025-11-08 00:00:00' as timestamp) - interval '9' hour
         and (coalesce (ci_hash_user_seq , 1)=1 and coalesce (mobile_hash_user_seq , 1)=1)
     )
+    , daily_active_users as
+    (select user_id, date_ymd_kst
+        from wippy_silver.daily_active_user_info
+        where first_approval_time between cast('2025-08-05 00:00:00'  as timestamp) - interval '9' hour and cast('2025-11-08 00:00:00' as timestamp) - interval '9' hour)
 , jelly_use as(
     select user_group
+            , first_approval_date
+            , date_diff('day',first_approval_date,date(date_ymd_kst))+1 as nth_day 
              , coalesce(max(case when price>0 then 1 end) over (partition by ci_hash),0) as has_purchased
             , row_number() over (partition by ci_hash order by ju.registered_time) as action_num
             , case when jb.registered_time is not null then
@@ -27,7 +33,7 @@ with users as
                     else description end as description
             , coalesce(sum((case when quantity<0 then abs(quantity) end)) over (partition by ju.user_id order by ju.registered_time),0) as cumul_jelly_use
     from wippy_bronze.billings_jellyuselog ju
-    join (select user_group,ci_hash, user_id from users) u on ju.user_id = u.user_id
+    join (select user_group,ci_hash, user_id, first_approval_date from users) u on ju.user_id = u.user_id
     left join wippy_bronze.billing_log jb on jb.user_id = u.user_id and jb.jelly_use_log_id = ju.id
     where description not like '%김수현%' and description not like '%주병규%' and description not like '%전지원%'
 )
@@ -40,19 +46,32 @@ select j.*
                 when j.description like '%채추추%' or j.description like '%추추%' or j.description like '%추가 추천%' then '추추'
                 when j.description like '%프까%' or j.description like '%프로필 오픈%' then '프까'
                 when j.description like '%채팅%' and j.description not like '%DM%' then '채팅'
-                when j.description like '%프로필 평가%' or j.description like '%닮은꼴%' or j.description like '%설문%' or j.description like '%리포트%' then '리포트'
+                when j.description like '%프로필 평가%' or j.description like '%닮은꼴%' or j.description like '%리포트%' then '리포트'
                 when j.description like '%보이스톡%'  then '보이스톡'
                 when j.description like '%쪽지%' or j.description like '%DM%' or j.description like '%친추%' then '친구 신청'
                 when j.description like '%타임어택%' then '타임어택'
                 when j.description like '%블라인드%' then '블라인드'
                 when j.description like '%동놀%' or j.description like '%오늘 볼래%' then '동네약속'
         else null end as use_type
+        , da.date_ymd_kst as active_date
     from jelly_use j
     left join first_purchase_exp p on p.ci_hash = j.ci_hash
-where j.action_num <= p.action_num + 10 or p.action_num is null
+    left join daily_active_users da on da.user_id = j.user_id and date(da.date_ymd_kst) = date(j.date_ymd_kst)
+where (j.action_num <= p.action_num + 10 or p.action_num is null)
+and j.nth_day <= 30
 
                                                          )
-select * from summary
+
+select user_group, get_referral_reward, has_purchased, ci_hash, first_approval_date
+        , max(nth_day) as last_jellyuse_date
+        , coalesce(sum(price),0) as purchase_amount
+        , count(distinct active_date) as active_days
+from summary
+group by 1,2,3,4,5
+
+-- select * from summary
+-- where ci_hash = '0c79adb62a83dd580cbebe7edd4de446cb17ae6f759f854cef686b2ed68c91aa'
+-- group by 1
 
 -- group by 2,3,4
 
