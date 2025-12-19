e15d28119c35a9fd62163bbba9c10b2ecb585855
Jaemin Seol <43691888+JAEMINSEOL@users.noreply.github.com>
2025-12-19 10:49:53 +0900

    Update wp_4744_연관추_AB.sql

--- wp_4744_연관추_AB.sql
+++ wp_4744_연관추_AB.sql
@@ -1,29 +1,22 @@
-create table da_adhoc.exp_wp_4744 as
-select distinct user_id, exp_props['WP-4744'] as exp_var
-from wippy_bronze.wippy_ubl
-where date(date_ymd_kst)  > date('2025-11-27')
-
 select *
      , num_profile_opened_users_today *100.0 / num_today_rcmd_users as rate_profile_today
-      , num_chat_request_users_today *100.0 / num_today_rcmd_users as rate_request_today
+      , num_chat_requested_users_today *100.0 / num_today_rcmd_users as rate_request_today
         , num_profile_opened_users_related *100.0 / num_related_rcmd_users as rate_profile_related
+        , num_profile_opened_users_related *100.0 / num_profile_opened_users_today as rate_profile_related_per_today
 from (select date_ymd_kst
+            , gender
            , exp_var
            , count(user_id)                                                                             as num_users
            , count(case when channel_id = 21 then user_id end)                                          as num_today_rcmd_users
            , count(case when rcmd_type = 2060 then user_id end)                                         as num_related_rcmd_users
-           , count(case
-                                when channel_id = 21 and not rcmd_type = 2060 and profile_open_time is not null
-                                    then user_id end)                                                            as num_profile_opened_users_today
-           , count(case
-                                when channel_id = 21 and not rcmd_type = 2060 and friend_request_time is not null
-                                    then user_id end)                                                            as num_chat_request_users_today
-           , count(case
-                                when rcmd_type = 2060 and profile_open_time is not null
-                                    then user_id end)                                                            as num_profile_opened_users_related
+           , count(case when channel_id = 21 and not rcmd_type = 2060 and profile_open_time is not null then user_id end)   as num_profile_opened_users_today
+           , count(case when channel_id = 21 and not rcmd_type = 2060 and friend_request_time is not null then user_id end) as num_chat_requested_users_today
+           , count(case when rcmd_type = 2060 and profile_open_time is not null then user_id end)                           as num_profile_opened_users_related
+            , count(case when rcmd_type = 2060 and friend_request_time is not null then user_id end)                         as num_chat_requested_users_related
       from (select r.user1_id as user_id,
                    u.exp_var,
                    r.user2_id,
+                   r.user1_gender as gender,
                    r.channel_id,
                    r.rcmd_type,
                    r.date_ymd_kst,
@@ -32,8 +25,9 @@ from (select date_ymd_kst
             from (select *
                   from wippy_silver.hourly_merged_rcmd_log
                   where date_ymd_kst > '2025-11-27'
-                    and (rcmd_type = 2060 or channel_id = 21)) r
-                     join (select *
+                    and (rcmd_type = 2060 or channel_id = 21)
+                    and impression_time is not null) r
+            join (select *
                            from da_adhoc.exp_wp_4744
                            where exp_var is not null) u on u.user_id = r.user1_id
 --                      left join (select quantity,
@@ -44,13 +38,9 @@ from (select date_ymd_kst
 --                                 where date (date_ymd_kst) > date('2025-11-27')
 --             and quantity<=0) j on j.rcmd_id = r.rcmd_id
            )
-group by 1, 2
-order by 2, 1 )
-order by 1,2
-
-
-
-
+group by 1, 2, 3
+order by 3, 2, 1 )
+order by 3,2,1
 
 
 
