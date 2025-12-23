b31b6b307921b3cccd2eb5ae5ecd02563cc1c601
Jaemin Seol <43691888+JAEMINSEOL@users.noreply.github.com>
2025-12-23 16:19:58 +0900

    Update [WIPPY] rcmd_count_from_new_join.sql

--- [WIPPY] rcmd_count_from_new_join.sql
+++ [WIPPY] rcmd_count_from_new_join.sql
@@ -2,12 +2,13 @@
 select user2_gender as gender
         -- , hour_kst
         , relative_score_grade
-        , min
+        , min*5 as min
         , avg(rcmd_users) as rcmd_users
         , avg(cumul_rcmd_users) as cumul_rcmd_users
         , avg(cumul_rcmd_response_users) as cumul_rcmd_response_users
         , avg(cumul_rcmd_request_users) as cumul_rcmd_request_users
         , avg(cumul_rcmd_2way_response_users) as cumul_rcmd_2way_response_users
+        , avg(cumul_rcmd_2way_response_users) *100.0 / avg(cumul_rcmd_response_users) as rcmd_2way_response_rate
         , count(distinct user2_id) as user2_cnt
 from (select *
            , sum(rcmd_users) over (partition by user2_id order by min rows between unbounded preceding and current row) as cumul_rcmd_users
@@ -15,11 +16,11 @@ from (select *
            , sum(rcmd_request_users) over (partition by user2_id order by min rows between unbounded preceding and current row) as cumul_rcmd_request_users
            , sum(rcmd_2way_response_users) over (partition by user2_id order by min rows between unbounded preceding and current row) as cumul_rcmd_2way_response_users
       from (select rc.user2_id
-                , floor(rc.user2_relative_score/10) +1 as relative_score_grade
+                , floor(rc.user2_relative_score/30) +1 as relative_score_grade
                 , rc.user2_gender
                 , rc.date_ymd_kst
                 , rc.hour_kst
-                , date_diff('minute', cast(u.approval_time as timestamp), cast(rc.registered_time as timestamp)) as min
+                , floor(date_diff('minute', cast(u.approval_time as timestamp), cast(rc.registered_time as timestamp))/5) as min
                 , coalesce(count(distinct user1_id),0) as rcmd_users
                 , coalesce(count(distinct case when rc.like_time is not null then user1_id end),0) as rcmd_response_users
                 , coalesce(count(distinct case when rr.like_time is not null or rr.dislike_time is not null then user1_id end),0) as rcmd_2way_response_users
@@ -30,7 +31,8 @@ from (select *
                     where date_ymd_kst >= '2025-12-01') rc
                 join
                 (select * from wippy_dump.accounts_user
-                    where date(first_approval_time + interval '9' hour) between date('2025-12-01') and date('2025-12-20') ) u
+                    -- where date(first_approval_time + interval '9' hour) between date('2025-12-01') and date('2025-12-20')
+                    where first_approval_time >= cast('2025-12-01' as timestamp) - interval '9' hour) u
                     on rc.user2_id = u.id and date (rc.date_ymd_kst) <= date_add('day',1, date (u.date_ymd_snapshot))
                 left join
                 (select user1_id as user_id, user2_id, registered_time, like_time, dislike_time from wippy_silver.hourly_merged_rcmd_log
@@ -42,7 +44,7 @@ from (select *
                     on (ch.owner_id = rc.user1_id and ch.invited_user_id = rc.user2_id) and ch.registered_time >= rc.registered_time
             
             group by 1, 2, 3, 4, 5,6)
-            where min <= 60*24 and min >= 0
+            where min*5 <= 60*24 and min >= 0
             )
 where user2_gender=1
     and relative_score_grade <=10
