3438dcc2f1c253534a7946d2b024c5322d5ec1fb
Jaemin Seol <43691888+JAEMINSEOL@users.noreply.github.com>
2025-12-29 18:50:26 +0900

    Update [WIPPY] wp_4378_남성_탈퇴방어_초기효과분석.sql

--- [WIPPY] wp_4378_남성_탈퇴방어_초기효과분석.sql
+++ [WIPPY] wp_4378_남성_탈퇴방어_초기효과분석.sql
@@ -1,27 +1,54 @@
-select date_ymd_kst, user_type, response
-        , count(distinct user_id) as users
-        from(
-select v.date_ymd_kst, v.user_id, json_extract_scalar(v.extra, '$.reward_type') as user_type, response
-from(
-    SELECT user_id,date_ymd_kst,navigations,event_type,extra
-    FROM wippy_bronze.wippy_ubl
-    where date_ymd_kst >= '2025-12-18'
-    and contains(navigations,'user_delete_reward')
-    and event_type in ('view_content')
-    and json_extract_scalar(extra, '$.reward_type') in ('MALE_HAS_BILLING','MALE_TOP_30')
-    and user_id is not null
-) v
-join(select * from(
-    SELECT user_id,date_ymd_kst,navigations,event_type,event_props['object_value'] as response
+-- select date_ymd_kst, user_type, response
+--         , count(distinct user_id) as users
+--         from(
+select * from(
+select coalesce(date(c.server_access_time),date(d.server_access_time)) as date_ymd_kst, v.user_id, json_extract_scalar(v.extra, '$.reward_type') as user_type
+        , case when c.server_access_time is not null then 'continue' when d.server_access_time is not null then 'delete' end as response
+        ,c.server_access_time as continue_server_access_time
+        ,d.server_access_time as delete_server_access_time
+        ,r.server_access_time as last_server_access_time
+        , date_diff('hour',coalesce(c.server_access_time,d.server_access_time),coalesce(d.server_access_time,cast('2030-12-29 8:06:17' as timestamp))) as retention_max
+from(select * from (SELECT user_id, date_ymd_kst, navigations, event_type, extra
+                    ,row_number() over (partition by user_id order by server_access_time) as rn
+                    FROM wippy_bronze.wippy_ubl
+                    where date_ymd_kst >= '2025-12-18'
+                      and contains(navigations, 'user_delete_reward')
+                      and event_type in ('view_content')
+                      and json_extract_scalar(extra, '$.reward_type') in ('MALE_HAS_BILLING', 'MALE_TOP_30')
+                      and user_id is not null
+                    ) where rn=1
+    ) v
+left join(select * from(
+    SELECT user_id,date_ymd_kst,navigations,server_access_time,event_type,event_props['object_value'] as response
             ,row_number() over (partition by user_id order by server_access_time) as rn
     FROM wippy_bronze.wippy_ubl
     where date_ymd_kst >= '2025-12-18'
     and contains(navigations,'user_delete_reward')
     and event_type  = 'click'
+    and event_props['object_value'] = 'continue'
     ) where rn=1
--- and json_extract_scalar(extra, '$.reward_type') in ('MALE_HAS_BILLING','MALE_TOP_30')
-) c on v.user_id = c.user_id and v.date_ymd_kst = c.date_ymd_kst
+    ) c on v.user_id = c.user_id
+left join(select * from(
+    SELECT user_id,date_ymd_kst,navigations,server_access_time,event_type,event_props['object_value'] as response
+            ,row_number() over (partition by user_id order by server_access_time desc) as rn
+    FROM wippy_bronze.wippy_ubl
+    where date_ymd_kst >= '2025-12-18'
+    and contains(navigations,'user_delete_reward')
+    and event_type  = 'click'
+    and event_props['object_value'] = 'delete'
+    ) where rn=1
+    ) d on v.user_id = d.user_id
+join(select * from(
+    select user_id, date_ymd_kst, server_access_time
+            , row_number() over (partition by user_id order by server_access_time desc) as rn
+    from wippy_bronze.wippy_ubl
+    where date_ymd_kst >= '2025-12-18'
+    ) where rn=1
+    ) r on r.user_id = v.user_id
+
+-- where c.server_access_time is null
+order by 3,4
 )
 where response is not null
-group by 1,2,3
-order by 1,2,3
+-- group by 1,2,3
+-- order by 1,2,3
