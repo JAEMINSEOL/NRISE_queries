f58217071306597ab569710525edd0a3af63ace9
Jaemin Seol <43691888+JAEMINSEOL@users.noreply.github.com>
2026-01-02 17:27:03 +0900

    Update [WIPPY] wp_4852_vana_reward.sql

--- [WIPPY] wp_4852_vana_reward.sql
+++ [WIPPY] wp_4852_vana_reward.sql
@@ -1,22 +1,34 @@
-SELECT n.*
-        , date(n.approval_time_kst) as approval_date
-        , u.relative_score
-        , u.location
-        , case when vc.user_id is not null then 1 else 0 end as get_vana_coupon
-        , vu.counts as used_vana_coupons
-FROM
-    (select user_id, gender
+with rcmd_partition as
+    (select user1_id, user2_id, registered_time,impression_time,like_time,dislike_time,friend_request_time,other_accepted_time,joined_time
+     from wippy_silver.hourly_merged_rcmd_log
+    where date_ymd_kst between '2025-12-15' and '2026-01-01'
+    ),
+    user_info as (select user_id, gender
+        , first_approval_time
         , first_approval_time + interval '9' hour as approval_time_kst
         , last_active_at + interval '9' hour as last_active_time_kst
         , date_diff('hour', first_approval_time, last_active_at) as retention_hour
     from wippy_silver.user_activation_metrics
-    where first_approval_time >= cast ('2025-12-20 00:00:00' as timestamp) - interval '9' hour
-    ) n
+    where first_approval_time >= cast ('2025-12-15 00:00:00' as timestamp) - interval '9' hour
+    )
+
+SELECT n.*
+        , date(n.approval_time_kst) as approval_date
+        , u.relative_score
+        , u.location
+        , vc.issue_cnt
+        , check_coupon_box
+        , vc.redeem_cnt
+        , vu.counts as used_vana_coupons
+        , d1_retention, d2_4_retention, d5_7_retention, w1_retention
+        , rcmd.*
+FROM user_info n
 join (select id, gender, relative_score, location
       from wippy_dump.accounts_user
         where relative_score<100
       ) u on u.id = n.user_id
-left join (SELECT distinct user_id FROM
+left join (SELECT user_id, count(issued_at_kst) as issue_cnt, count(redeemed_at_kst) as redeem_cnt
+                FROM
                                                          (SELECT id,
          created_at + INTERVAL '9' HOUR AS issued_at_kst,
          received_at + INTERVAL '9' HOUR AS redeemed_at_kst,
@@ -25,13 +37,43 @@ left join (SELECT distinct user_id FROM
          item_id
   FROM wippy_dump.mobile_coupon_box
   WHERE created_at >= TIMESTAMP '2025-12-22 00:00:00' - INTERVAL '9' HOUR
-                                                         and received_at is not null)
---                                                          wippy_bronze.wippy_ubl
---             where date_ymd_kst >= '2025-12-20'
--- --             and contains(navigations,'home')
---             and (event_props['banner_title'] like '%커피 4잔을%' or contains(navigations, 'onboarding_step_mobile_coupon')
---                                                          or (contains(navigations, 'mobile_coupon') and event_type = 'click'))
+                                                         )
+                                                         group by 1
             ) vc on vc.user_id = n.user_id
+left join (select n.user_id
+                    , count(distinct case when date_diff('day',date(n.approval_time_kst),ub.date_ymd_kst)=1 then 1 end) as d1_retention
+                    , count(distinct case when date_diff('day',date(n.approval_time_kst),ub.date_ymd_kst) between 2 and 4 then 1 end) as d2_4_retention
+                    , count(distinct case when date_diff('day',date(n.approval_time_kst),ub.date_ymd_kst) between 5 and 7 then 1 end) as d5_7_retention
+                    , count(distinct case when date_diff('day',date(n.approval_time_kst),ub.date_ymd_kst) between 8 and 14 then 1 end) as w1_retention
+                    , max(check_coupon_box) as check_coupon_box
+            from (select user_id, date(date_ymd_kst) as date_ymd_kst
+                    , max (case when (contains (navigations, 'mobile_coupon')) then 1 else 0 end) as check_coupon_box
+                    from wippy_bronze.wippy_ubl
+                    where date_ymd_kst between '2025-12-15' and '2026-01-01'
+                    group by 1,2) ub
+            join user_info n on ub.user_id = n.user_id
+            group by 1) ub on ub.user_id = n.user_id
+left join (select user1_id
+                , r1.regi as rcmd_regi, imp as rcmd_imp, resp as rcmd_resp, req as rcmd_req, r1.mat as rcmd_match, r2.regi as rcmd_other_regi,  r2.mat as rcmd_other_match
+                from (select r1.user1_id
+                        , count (distinct case when date_diff('hour',n.first_approval_time,r1.registered_time) <= 24 then r1.user2_id end) as regi
+                        , count (distinct case when date_diff('hour',n.first_approval_time,r1.impression_time) <= 24 then r1.user2_id end) as imp
+                        , count (distinct case when date_diff('hour',n.first_approval_time,r1.like_time) <= 24 or date_diff('hour',n.first_approval_time,r1.dislike_time) <= 24 then r1.user2_id end) as resp
+                        , count (distinct case when date_diff('hour',n.first_approval_time,r1.friend_request_time) <= 24 then r1.user2_id end) as req
+                        , count (distinct case when date_diff('hour',n.first_approval_time,r1.other_accepted_time) <= 24 then r1.user2_id end) as mat
+                        from rcmd_partition r1
+                        join user_info n on n.user_id = r1.user1_id
+                        group by 1) r1
+                left join (select r1.user2_id
+                        , count (distinct case when date_diff('hour',n.first_approval_time,r1.registered_time) <= 24 then r1.user2_id end) as regi
+--                         , count (distinct case when date_diff('hour',n.first_approval_time,r1.impression_time) <= 24 then r1.user2_id end) as imp
+--                         , count (distinct case when date_diff('hour',n.first_approval_time,r1.like_time) <= 24 or date_diff('hour',n.first_approval_time,r1.dislike_time) <= 24 then r1.user2_id end) as resp
+--                         , count (distinct case when date_diff('hour',n.first_approval_time,r1.friend_request_time) <= 24 then r1.user2_id end) as req
+                        , count (distinct case when date_diff('hour',n.first_approval_time,r1.other_accepted_time) <= 24 then r1.user2_id end) as mat
+                        from rcmd_partition r1
+                        join user_info n on n.user_id = r1.user2_id
+                        group by 1) r2 on r1.user1_id = r2.user2_id
+                )rcmd on rcmd.user1_id = n.user_id
 left join (SELECT user_id, COUNT (distinct coupon_id) AS counts
             FROM ( SELECT id as coupon_id, created_at, source_id, used_at, assigned_user_id as user_id
                                     FROM wippy_dump.mobile_coupon_code
