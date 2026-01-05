c317529e9d9a5fa5314e5aa0d448c3675a683088
Jaemin Seol <43691888+JAEMINSEOL@users.noreply.github.com>
2026-01-05 17:34:56 +0900

    Update [WIPPY] initial 추천 횟수 200으로 감소 AB test.sql

--- [WIPPY] initial 추천 횟수 200으로 감소 AB test.sql
+++ [WIPPY] initial 추천 횟수 200으로 감소 AB test.sql
@@ -21,6 +21,7 @@ select mod(r.user_id,2) as user_group, r.gender, approval_date
         
 from(
     select user2_id as user_id, user2_gender as gender
+            , max(user2_relative_score) as relative_score
             , count(distinct case when rcmd_type = 50000 then user1_id end) as initial_rcmd_cnts
             , count(distinct case when rcmd_type = 50000 and impression_time is not null then user1_id end) as initial_imp_cnts
             , count(distinct case when rcmd_type = 50000 and impression_time is not null and coalesce(like_time,dislike_time) is not null then user1_id end) as initial_resp_cnts
@@ -32,8 +33,14 @@ from(
     (select *, row_number() over (partition by user1_id, user2_id order by rcmd_type desc) as rn
     from rcmd_part r1
     join (select user1_id as user1_rcmd_id, user2_id as user2_rcmd_id, other_accepted_time as r2_other_accepted_time from rcmd_part) r2 on r2.user1_rcmd_id = r1.user1_id and r2.user2_rcmd_id = r1.user2_id
-    )
+    ) r
+        join (
+        select user_id, first_approval_time
+        from wippy_silver.user_activation_metrics
+        where date(first_approval_time+interval '9' hour) between date('2025-12-30') and date('2026-01-03')
+        ) u on r.user2_id = u.user_id
     where rn=1
+     and date_diff('hour',u.first_approval_time,r.registered_time) <= 24
     group by 1,2
 ) r
 join (
